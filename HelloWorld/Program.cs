f492c325764682f2856a130a8af217e274eef2e3
Zachary Haggerty <zhaggerty24@gmail.com>
2022-07-24 01:41:56 -0400

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZachStinks
+{
+    class program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hi. Enter your name below");
+            var teacherName = Console.ReadLine();
+            Console.WriteLine("Hello, Adrian! How are you today");
+            
+
+       
+        }
+    }
+} 
